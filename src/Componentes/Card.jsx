6f5986f685d29ca2c813b2eccbb8a542e82b2dbe
unknown <carneiroleila@gmail.com>
2022-10-26 12:14:52 -0300

    card component img changes

--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -1,9 +1,10 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-export default function Card(id, name, cost){
+export default function Card(id, img, name, cost){
     return(
         <div>
+            <img src={img[0]} alt='Image not found' />
             <h2>{name}</h2>
             <h2>${cost}</h2>
             <Link to={`/product/${id}`}>  {/* route placeholder */}
