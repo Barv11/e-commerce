b713b8b756ecd49d1dfd092d3dc27a32644e3140
unknown <carneiroleila@gmail.com>
2022-10-27 16:02:18 -0300

    fixed deleted component in Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Card from "../Card/Card";
 import CarouselHeader from "./Carousel/Carousel"
-import CarouselProduct from '../CarouselProduct'
 import ProductSlider from '../ProductSlider'
 
 export default function Home() {
