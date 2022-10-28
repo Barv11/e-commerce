9d359c492f1a81a68d94e76ec06cde4cdbcea9b2
Bryan Ramos Vargas <matematica_bryan@hotmail.com>
2022-10-28 11:40:34 -0500

    cardDetail terminado con componente por producto

---
+++ src/components/CardDetail/DetailComponents/DetailCooler/DetailCooler.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export default function DetailCooler({details}) {
+    const {fans, RGB, consumo, tdp, disipasión, dimensions, compatibilidad} = details
+    // nivel de ruido
+  return (
+    <>
+      <span>
+        <b>Fans: </b>
+        {fans}
+      </span>
+      <span>
+        <b>Nidel de Ruido: </b>
+        {details["nivel de ruido"]}
+      </span>
+      <span>
+        <b>Luces: </b>
+        {RGB?"Yes":"none"}
+      </span>
+      <span>
+        <b>Consumo: </b>
+        {consumo}
+      </span>
+      <span>
+        <b>{"Potencia de diseño térmico (TPD)"}: </b>
+        {tdp}
+      </span>
+      <span>
+        <b>Disipación: </b>
+        {disipasión}
+      </span>
+      <span>
+        <b>Compatibilidad: </b>
+        <ul>
+              {compatibilidad?.map((el) => (
+                <li>{el}</li>
+              ))}
+            </ul>
+      </span>
+      <span>
+        <b>Dimensiones: </b>
+        <ul>
+            <li>Altura: {dimensions.altura}</li>
+            <li>Tamaño del cooler: {dimensions.tamaño_coolers}</li>
+        </ul>
+      </span>
+    </>
+
+  )
+}
---
+++ src/components/CardDetail/DetailComponents/DetailGabinete/DetailGabinete.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+
+export default function DetailGabinete({ details }) {
+  const {
+    dimensions,
+    weight,
+    cooling,
+    color,
+    material,
+    RGB,
+    connectivity,
+    power_supply_type,
+    mother_types,
+    max_length_psu,
+    max_length_gpu,
+  } = details;
+  // structure type
+  return (
+    <>
+      <span>
+        <b>Material: </b>
+        {material}
+      </span>
+      <span>
+        <b>Luces: </b>
+        {RGB ? "Yes" : "none"}
+      </span>
+      <span>
+        <b>Estructura: </b>
+        {details["structure type"]}
+      </span>
+      <span>
+        <b>Conectividad: </b>
+        <ul>
+          <li>
+            Disk:
+            <ul>
+              <li>{"HDD: " + connectivity.disk.hdd}</li>
+              <li>{"SSD: " + connectivity.disk.ssd}</li>
+              <li>{"Size: " + connectivity.disk.size_inch}</li>
+            </ul>
+          </li>
+        </ul>
+      </span>
+      <span>
+        <b>Enfriamiento: </b>
+        <ul>
+          <li>{"Front fans num: " + cooling.front_fans_num}</li>
+          <li>
+            {"Front fans dimension: "}
+            <ul>
+              {cooling.front_fans_dimension_mm?.map((el) => (
+                <li>{el}</li>
+              ))}
+            </ul>
+          </li>
+          <li>{"Back fans num: " + cooling.back_fans_num}</li>
+          <li>
+            {"Back fans num: "}
+            <ul>
+              {cooling.back_fans_dimension_mm?.map((el) => (
+                <li>{el}</li>
+              ))}
+            </ul>
+          </li>
+        </ul>
+      </span>
+      <span>
+        <b>Color: </b>
+        {color}
+      </span>
+      <span>
+        <b>Fuente de Alimentación: </b>
+        {power_supply_type}
+      </span>
+      <span>
+        <b>Tipo de Mother: </b>
+        {mother_types}
+      </span>
+      <span>
+        <b>Máxima longitud PSU: </b>
+        {max_length_psu}
+      </span>
+      <span>
+        <b>Máxima longitud GPU: </b>
+        {max_length_gpu}
+      </span>
+      <span>
+        <b>Peso: </b>
+        {weight}
+      </span>
+      <span>
+        <b>Dimensiones: </b>
+        {dimensions}
+      </span>
+    </>
+  );
+}
