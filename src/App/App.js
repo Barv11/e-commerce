c698c51e1d07f0935da60358d198eda020506005
Bryan Ramos Vargas <matematica_bryan@hotmail.com>
2022-10-27 20:44:40 -0500

    accept changes

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,6 @@
-import Navbar from "../components/Navbar/Navbar.jsx";
 import { Route } from "react-router-dom";
-<<<<<<< HEAD
-import Home from "../components/Home/Home";
-import CardDetail from "../components/CardDetail/CardDetail.jsx";
-=======
 import Home from "../page/home/Home";
 import CardDetail from "../components/CardDetail/CardDetail";
->>>>>>> 67143abc50af408f82b5fcada1f9f0ae1f330bf4
 
 function App() {
   return (
@@ -14,11 +8,7 @@ function App() {
       <Route exact path={"/"}>
         <Home />
       </Route>
-<<<<<<< HEAD
-      <Route path={'/product/:id'}>
-=======
       <Route path={"/product/id"}>
->>>>>>> 67143abc50af408f82b5fcada1f9f0ae1f330bf4
         <CardDetail />
       </Route>
     </>
