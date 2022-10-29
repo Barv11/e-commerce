73eca286c0bdb643d1879cac16543d1928f32642
Tobi Tortosa <tobiastortosa17@gmail.com>
2022-10-28 23:15:03 -0300

    added register page

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import Login from "../components/LoginForm/LoginForm";
 import Products from "../page/allProducts/AllProducts";
 import Home from "../page/home/Home";
+import Register from "../components/RegisterForm/Register";
 function App() {
   return (
     <Routes>
       <Route exact path={"/"} element={<Home />} />
       <Route exact path={"/login"} element={<Login />} />
+      <Route exact path={"/register"} element={<Register />} />
       <Route path={"/products"} element={<Products />} />
     </Routes>
   );
