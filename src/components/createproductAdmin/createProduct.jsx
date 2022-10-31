812b80b3900d6f0ecf9a935c08fe18eedcbcaf87
Bryan Ramos Vargas <matematica_bryan@hotmail.com>
2022-10-31 12:25:48 -0500

    agregando funcionalidad para subir detalles

--- src/components/createproductAdmin/createProduct.jsx
+++ src/components/createproductAdmin/createProduct.jsx
@@ -7,7 +7,54 @@ import { postProduct, getAllProductos } from "../../redux/actions/index";
 import Navbar from "../Navbar/Navbar";
 import styles from "./createProduct.module.css";
 
+// Lo moví acá para no ocupar visión (provisonal)
+
+//Cloudinary
+//   const [img, setimg] = useState("");
+//   const [loading, setLoading] = useState(false);
+
+//   const handleimg = async (e) => {
+//     const files = e.target.files
+//     const data = new FormData();
+//     data.append("file", files[0]);
+//     data.append("upload_preset", "imgs")
+//     setLoading(true);
+
+//     const res = await axios.post (
+//         "https://api.cloudinary.com/v1_1/juliap/img/upload",
+//         data
+//     )
+
+//     console.log(res)
+//     setimg(res.data.secure_url)
+
+//     setProduct({
+//       ...product,
+//       img: res.data.secure_url
+//     });
+//     setErrors(validate({
+//       ...product,
+//       img:res.data.secure_url
+//     }))
+//     setLoading(false)
+// }
+//ProductsController
+// const imgsCloudinary = async (img) =>{
+//          const files = img;
+//         const data = new FormData(); //datos que vamos a recibir
+//         data.append("file", files[0]);//los files q recibimos
+//         data.append("upload_preset", "imgs") //la carpeta donde lo vamos a subir
+
+//     const res = await axios.post (
+//         "https://api.cloudinary.com/v1_1/juliap/img/upload",
+//         data
+//     )
+//     return res.data.secure_url;
+// }
+
 function CreateProduct() {
+  // const allProducts = useSelector(state => state.adminProducts)
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   function validate(input) {
@@ -18,9 +65,9 @@ function CreateProduct() {
     if (!input.cost) {
       errors.cost = "Es Precio es requerido";
     }
-    //  if(!input.img){
-    //    errors.img = 'La img es requerida'
-    //  }
+    if (!input.img.length) {
+      errors.img = "La img es requerida";
+    }
     // if(!input.details){
     //   errors.details = 'La Descripción es requerida'
     // }
@@ -33,14 +80,6 @@ function CreateProduct() {
     }
     return errors;
   }
-
-  useEffect(() => {
-    dispatch(getAllProductos());
-  }, []);
-
-  // const allProducts = useSelector(state => state.adminProducts)
-
-  //  const navigate = useNavigate();
 
   const [product, setProduct] = useState({
     name: "",
@@ -81,70 +120,24 @@ function CreateProduct() {
     );
   };
 
-  //Cloudinary
-  //   const [img, setimg] = useState("");
-  //   const [loading, setLoading] = useState(false);
-
-  //   const handleimg = async (e) => {
-  //     const files = e.target.files
-  //     const data = new FormData();
-  //     data.append("file", files[0]);
-  //     data.append("upload_preset", "imgs")
-  //     setLoading(true);
-
-  //     const res = await axios.post (
-  //         "https://api.cloudinary.com/v1_1/juliap/img/upload",
-  //         data
-  //     )
-
-  //     console.log(res)
-  //     setimg(res.data.secure_url)
-
-  //     setProduct({
-  //       ...product,
-  //       img: res.data.secure_url
-  //     });
-  //     setErrors(validate({
-  //       ...product,
-  //       img:res.data.secure_url
-  //     }))
-  //     setLoading(false)
-  // }
-  //ProductsController
-  // const imgsCloudinary = async (img) =>{
-  //          const files = img;
-  //         const data = new FormData(); //datos que vamos a recibir
-  //         data.append("file", files[0]);//los files q recibimos
-  //         data.append("upload_preset", "imgs") //la carpeta donde lo vamos a subir
-
-  //     const res = await axios.post (
-  //         "https://api.cloudinary.com/v1_1/juliap/img/upload",
-  //         data
-  //     )
-  //     return res.data.secure_url;
-  // }
-
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(postProduct(product));
 
-    let add = {
-      ...product,
-      cost: parseInt(product.cost)
-    }
-
-
     setProduct({
       name: "",
       cost: "",
-      img: '',
+      img: "",
       // details: '',
       type: "",
       brand: "",
     });
-
-    //  navigate.push("/products");
   };
+
+  useEffect(() => {
+    return dispatch(getAllProductos());
+  }, []);
+
   console.log(product);
   return (
     <>
@@ -153,41 +146,44 @@ function CreateProduct() {
         <div className={styles.productContainer}>
           <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
             <h2 className={styles.titleForm}>Crear producto</h2>
-            <label htmlFor="name">Name</label>
+            <label htmlFor="name">Nombre</label>
             <input
               className={styles.formInput}
               type={"text"}
-              placeholder={"name"}
+              placeholder={"Ingrese un nombre"}
               name={"name"}
               value={product.name}
               onChange={(e) => handleInput(e)}
             />
             <br />
-            {errors.name && <p className={styles.textError}>{errors.name}</p>}
+            <p className={styles.textError}>{errors.name ? errors.name : ""}</p>
 
-            <label htmlFor="cost">Cost</label>
+            <label htmlFor="cost">Precio</label>
             <input
               className={styles.formInput}
               type={"number"}
-              placeholder={"cost"}
+              min={0}
+              placeholder={"Ingresar Precio"}
               name={"cost"}
               value={product.cost}
               onChange={(e) => handleInput(e)}
             />
             <br />
-            {errors.cost && <p className={styles.textError}>{errors.cost}</p>}
+            <p className={styles.textError}>{errors.cost ? errors.cost : ""}</p>
 
-            <label htmlFor="brand">Brand</label>
+            <label htmlFor="brand">Marca</label>
             <input
               className={styles.formInput}
               type={"text"}
-              placeholder={"brand"}
+              placeholder={"Ingresar Marca"}
               name={"brand"}
               value={product.brand}
               onChange={(e) => handleInput(e)}
             />
             <br />
-            {errors.brand && <p className={styles.textError}>{errors.brand}</p>}
+            <p className={styles.textError}>
+              {errors.brand ? errors.brand : ""}
+            </p>
 
             {/* <label htmlFor='stock'>Cantidad</label>
           <input className={styles.formInput}  type={'number'} placeholder={'Cantidad'} 
@@ -223,6 +219,7 @@ function CreateProduct() {
               name={"uploads"}
               className={styles.formInput}
               type={"text"}
+              value={product.img}
               placeholder={"Ingrese url de imagen"}
               onChange={(e) => handleInput(e)}
               // onChange={(e)=> handleimg(e)}
@@ -230,19 +227,111 @@ function CreateProduct() {
             {/* {loading? (<h8>Cargando imágenes...</h8>) : <img src={img} style={{width: "300px"}}/> } */}
             <br />
             {/* </FormGroup> */}
-            {errors.img && <p className={styles.textError}>{errors.img}</p>}
+            <p className={styles.textError}>{errors.img ? errors.img : ""}</p>
 
-            <label htmlFor="type">Type</label>
+            <label htmlFor="type">Tipo</label>
             <input
               className={styles.formInput}
               type={"text"}
-              placeholder={"type"}
+              placeholder={"Ingresar categoría"}
               name={"type"}
               value={product.type}
               onChange={(e) => handleInput(e)}
             />
             <br />
-            {errors.type && <p className={styles.textError}>{errors.type}</p>}
+            <p className={styles.textError}>{errors.type ? errors.type : ""}</p>
+
+            <label>Categorías</label>
+            <select name="type" className={styles.formInput}>
+              <option value="null">Elija...</option>
+              <option value="mother">Mother</option>
+              <option value="procesador">Procesador</option>
+              <option value="disco">Disco</option>
+              <option value="ram">Ram</option>
+              <option value="solido">Solido</option>
+              <option value="tarjeta">Tarjeta</option>
+              <option value="mouse">Mouse</option>
+              <option value="pantalla">Pantalla</option>
+              <option value="fuente">Fuente</option>
+              <option value="teclado">Teclado</option>
+              <option value="auricular">Auricular</option>
+              <option value="gabinete">Gabinete</option>
+              <option value="cooler">Cooler</option>
+            </select>
+
+            <label>Luces RGB</label>
+            <select name="RGB" className={styles.formInput}>
+              <option>true</option>
+              <option>false</option>
+            </select>
+
+            <label>Color</label>
+            <input
+              className={styles.formInput}
+              type={"text"}
+              placeholder={"Ingrese un color"}
+              name={"color"}
+              // value={product.name}
+              // onChange={(e) => handleInput(e)}
+            />
+
+            <label>Conectividad</label>
+            <input
+              className={styles.formInput}
+              type={"text"}
+              placeholder={"Ingrese la conectividad"}
+              name={"connectivity"}
+              // value={product.name}
+              // onChange={(e) => handleInput(e)}
+            />
+
+            <label>Tipo de teclado</label>
+            <input
+              className={styles.formInput}
+              type={"text"}
+              placeholder={"Ingrese tipo de teclado"}
+              name={"tipo_de_teclado"}
+              // value={product.name}
+              // onChange={(e) => handleInput(e)}
+            />
+
+            <label>Tipo de Mecanismo</label>
+            <input
+              className={styles.formInput}
+              type={"text"}
+              placeholder={"Ingrese tipo de mecanismo"}
+              name={"tipo_de_mecanismo"}
+              value={product.name}
+              // onChange={(e) => handleInput(e)}
+            />
+
+            <label>Dimensiones</label>
+            <label>{"Ancho (mm):"}<input
+              className={styles.formInput}
+              type={"number"}
+              placeholder={"Ingrese dimensiones"}
+              name={"dimensions"}
+              // value={product.name}
+              // onChange={(e) => handleInput(e)}
+            /></label>
+            <label>{"Profundidad (mm):"}<input
+              className={styles.formInput}
+              type={"number"}
+              placeholder={"Ingrese dimensiones"}
+              name={"dimensions"}
+              // value={product.name}
+              // onChange={(e) => handleInput(e)}
+            /></label>
+            <label>{"Alto (mm):"}<input
+              className={styles.formInput}
+              type={"number"}
+              placeholder={"Ingrese dimensiones"}
+              name={"dimensions"}
+              // value={product.name}
+              // onChange={(e) => handleInput(e)}
+            /></label>
+
+
             <div className={styles.containerBtn}>
               <button className={styles.btn} type="submit">
                 Crear
