161d5107bb662c56f4ba530923f84bdbdcf2c273
Bryan Ramos Vargas <matematica_bryan@hotmail.com>
2022-11-01 12:54:51 -0500

    edité algo que dejé hardcodeado

--- src/components/CardDetail/DetailComponents/DetailMother/DetailMother.jsx
+++ src/components/CardDetail/DetailComponents/DetailMother/DetailMother.jsx
@@ -3,68 +3,12 @@ import React from "react";
 export default function DetailMother({ details }) {
   const { connectivity, Socket, energy, Sound, Memory, Dimensions } = details;
 
-  const mother = {
-    type: "mother",
-    brand: "AMD",
-    name: "Mother Asrock X570 Phantom Gaming 4 AM4 PCIe Gen4",
-    cost: 36800,
-    img: [
-      "https://cdn.pixabay.com/photo/2017/06/24/14/13/buenos-aires-2437858_960_720.jpg",
-    ],
-    details: {
-      connectivity: {
-        "Cantidad De Slot Pci-e 16X": 2,
-        "Cantidad De Slot Pci-e 1X": 2,
-        "Tecnologia Multi Gpu": "Crossfire",
-        "Puertos Sata": 8,
-        "Salida Vga": 0,
-        "Salida HDMI": 1,
-        "Salida Dvi": 0,
-        "Salidas Display Ports": 1,
-        "Cantidad de Slot M.2 Totales": 2,
-        "Placa Wifi Integrada": "No",
-        "Placa de Red": "Gigabit LAN 10/100/1000 Mb/s",
-        "Puerto Ps/2": "No",
-        "Puertos Usb 2.0 Traseros": 4,
-        "Puertos Usb 3.0 Traseros": 2,
-        "Puertos Usb 3.1 Traseros": 0,
-        "Puertos Usb 3.2 Traseros": 0,
-        "Puertos Usb Type-c": 0,
-        "Cantidad De Slot Pci-e 4X": 0,
-        "Cantidad De Slot M.2 Sata": 0,
-        "Cantidad De Slot M.2 Nvme": 2,
-      },
-      Socket: [
-        "AM4 APU 2th Gen",
-        "AM4 Ryzen 3th Gen",
-        "AM4 APU 3th Gen",
-        "AM4 Ryzen 4th Gen",
-        "AM4 Ryzen 2th Gen",
-        "AM4 APU 5000",
-      ],
-      energy: {
-        "Watts Máximos Para Cpu": 105,
-        "Conectos Cpu 4Pines": 1,
-        "Conector Cpu 4Pines Plus": 1,
-        "Conector 24Pines": 1,
-        Consumo: "35w",
-        ProcesadorIntegrado: "No",
-      },
-      Sound: {
-        "Placa De Sonido": "7.1 Realtek ALC 1200",
-      },
-      Memory: {
-        Ddr4: 4,
-      },
-      Dimensions: "ATX",
-    },
-  };
   return (
     <>
       <span>
         <b>Conectividad:</b>
         <ul>
-          {Object.entries(mother.details.connectivity).map(
+          {Object.entries(connectivity).map(
             ([key, value] , el) => (
               <li key={el}>{key + ": " + value}</li>
             )
@@ -82,7 +26,7 @@ export default function DetailMother({ details }) {
       <span>
         <b>Energía: </b>
         <ul>
-          {Object.entries(mother.details.energy).map(
+          {Object.entries(energy).map(
             ([key, value], el) => (
               <li key={el}>{key + ": " + value}</li>
             )
@@ -92,7 +36,7 @@ export default function DetailMother({ details }) {
       <span>
         <b>Sonido: </b>
         <ul>
-          {Object.entries(mother.details.Sound).map(
+          {Object.entries(Sound).map(
             ([key, value], el) => (
               <li key={el}>{key + ": " + value}</li>
             )
@@ -102,7 +46,7 @@ export default function DetailMother({ details }) {
       <span>
         <b>Memoría: </b>
         <ul>
-          {Object.entries(mother.details.Memory).map(
+          {Object.entries(Memory).map(
             ([key, value], el) => (
               <li key={el}>{key + ": " + value}</li>
             )
