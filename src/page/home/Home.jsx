54672735e0cb9b849bf7c40d150d8e63c880e08b
Tobi Tortosa <tobiastortosa17@gmail.com>
2022-11-03 15:32:10 -0300

    conection to cart

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -3,19 +3,28 @@ import Carrusel from "../../components/Carruseles/Carousel/Carousel";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer.jsx";
 import { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser, addCartProduct } from "../../redux/actions";
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const userFound = useSelector((state) => state.userFound);
+
   const [user, setUser] = useState(
-    JSON.parse(
-      localStorage.getItem("user") ||
-        JSON.stringify({
-          logged: false,
-          token: "",
-        })
-    )
+    JSON.parse(localStorage.getItem("user") || "[]")
   );
 
-  localStorage.setItem("user", JSON.stringify(user));
+  const [products, setProducts] = useState(
+    JSON.parse(localStorage.getItem("products") || "[]")
+  );
+
+  useEffect(() => {
+    if (user.logged) {
+      dispatch(getUser(user.token));
+      dispatch(addCartProduct(userFound.id, products));
+    }
+  }, []);
 
   return (
     <div>
