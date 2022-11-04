d3f63f949ea14eb3d2b57a1d005bb19bae405bcd
sebastian <sebastyandx@hotmail.com>
2022-11-03 22:43:14 -0300

    andando

---
+++ src/components/PayButton/Paybutton.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+/* import { useSelector } from "react-redux"; */
+
+export default function Paybutton(cartItem) {
+  /* const user = useSelector((state) => state.auth); */
+  const handleCheckout = () => {
+    axios
+      .post(
+        "http://localhost:3001/create-checkout-session",
+        cartItem
+        /* userId: user.id */
+      )
+      .then((res) => {
+        if (res.data) {
+          window.location.href = res.data;
+        }
+      })
+      .catch((err) => console.log(err.message));
+  };
+  return (
+    <div>
+      <button onClick={() => handleCheckout()}>Check Out</button>
+    </div>
+  );
+}
