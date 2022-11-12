d9d8a9c80cdfccce0a3f410c68b6b5c24cafb56d
Bryan Ramos Vargas <matematica_bryan@hotmail.com>
2022-11-12 17:00:04 -0500

    avance

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
+import s from "./About.module.css"
 
 export default function About() {
   return (
     <>
         <Navbar />
-        
+        <main className={s.container}>
+          <h1>Hola</h1>
+        </main>
         <Footer />
     </>
   )
