41ee31cfc041ce52b42d8887f4717361345f0b1a
Bryan Ramos Vargas <matematica_bryan@hotmail.com>
2022-11-15 23:26:34 -0500

    estilos

--- src/page/Admin/AdminDash.jsx
+++ src/page/Admin/AdminDash.jsx
@@ -20,27 +20,27 @@ export default function AdminDash() {
                 <div>
                     <Link style={{ textDecoration: 'none' }} to={'/users'}>
                         <div className='categoriasContainer'>
-                        <h2 className='categoriasTxt'>Lista de Usuarios</h2>
+                        <h2 className='categoriasTxt'>Lista de Usuarios <i class="uil uil-users-alt"></i></h2>
                         </div>
                     </Link>
                     <Link style={{ textDecoration: 'none' }} to={'/create'}>
                         <div className='categoriasContainer'>
-                        <h2 className='categoriasTxt'>Crear Producto</h2>
+                        <h2 className='categoriasTxt'>Crear Producto <i class="uil uil-file-plus-alt"></i></h2>
                         </div>
                     </Link>
                     <Link style={{ textDecoration: 'none' }} to={'/inventory'}>
                         <div className='categoriasContainer'>
-                        <h2 className='categoriasTxt'>Inventario</h2>
+                        <h2 className='categoriasTxt'>Inventario <i class="uil uil-clipboard-notes"></i></h2>
                         </div>
                     </Link>
                     <Link style={{ textDecoration: 'none' }} to={'/orden'}>
                         <div className='categoriasContainer'>
-                        <h2 className='categoriasTxt'>Ordenes de Compra</h2>
+                        <h2 className='categoriasTxt'>Ordenes de Compra <i class="uil uil-shopping-cart"></i></h2>
                         </div>
                     </Link>
                     <Link style={{ textDecoration: 'none' }} to={'/trash'}>
                         <div className='categoriasContainer'>
-                        <h2 className='categoriasTxt'>Ver Papelera</h2>
+                        <h2 className='categoriasTxt'>Ver Papelera <i class="uil uil-trash-alt"></i></h2>
                         </div>
                     </Link>
                 </div>
